ef7a14a6628b01797c35f5d2d67ca6240dd72f8a
Minoud <96798701+Minoud@users.noreply.github.com>
2023-02-25 12:03:43 +0100

    Update code.ml

--- code.ml
+++ code.ml
@@ -1,6 +1,5 @@
 type point = {x:int; y;int};
 type case = {pos:point; couleur:int};
-type grid = {t_x:int ; t_y:int};
+type grid = {t_x:int ; t_y:int; points:point list};
 
-
-let cree_grid x y = {t_x=x;t_y=y}
+let init_grid x y liste = {t_x=x;t_y=y;cases=liste}
