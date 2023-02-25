0392decd89da26b8043a18deeafd8277fefe92af
Minoud <96798701+Minoud@users.noreply.github.com>
2023-02-25 12:33:18 +0100

    Update code.ml

--- code.ml
+++ code.ml
@@ -2,4 +2,14 @@ type point = {x:int; y:int};
 type case = {pos:point; couleur:int};
 type grid = {t_x:int ; t_y:int; points:case array};
 
-let init_grid x y liste = {t_x=x;t_y=y;cases=liste}
+let init_array x y : case array =
+  let tab = Array.make (x*y) {pos={x=0;y=0};couleur=0} in
+  let compt= ref 0 in
+  let stadex = ref 0 in
+  for i=0 to (x*y)-1 do
+    tab.(i)<-{pos={x=(!stadex);y=(!compt)};couleur=0};
+    compt:=!compt+1;
+    if !compt>(y-1) then (stadex:=!stadex+1; compt:=0);
+    done;
+  tab
+;;
