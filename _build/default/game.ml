b8d62223fefa6120868c279b107f8fbf70d82ebb
Théo Bossert <theo67.jeu@gmail.com>
2023-02-25 12:37:22 +0100

    affichage grille

--- _build/default/game.ml
+++ _build/default/game.ml
@@ -1,16 +1,29 @@
 open Raylib
+open Random
 
-let setup () =
-  Raylib.init_window 500 500 "Flood it";
-  Raylib.set_target_fps 60
+let t = [|Color.blue; Color.yellow; Color.orange; Color.green; Color.red; Color.green|]
+let g = Array.make ((14*14) - 1) Color.white
+
+let colors () =
+  for i = 0 to (14*14) - 1 do
+    self_init ();
+    g.(i) <- t.(int 5)
+  done
 
 let grid () =
-  let t = [|Color.blue; Color.yellow; Color.orange; Color.green; Color.red; Color.green|] in
   for i = 0 to 14 do
     for j = 0 to 14 do
-      draw_rectangle (100 + j*16) (100 + 16*i) 16 16 t.(Random.int 5)
-    done
+        draw_rectangle (100 + j*16) (100 + i*16) 16 16 g.(i*j)
+    done;
   done
+
+let setup () =
+  init_window 500 500 "Flood it";
+  set_target_fps 60;
+  colors ();
+  begin_drawing ();
+  grid ();
+  end_drawing ()
 
 let rec loop () =
   if window_should_close () then close_window ()
