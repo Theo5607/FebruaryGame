64ae0103da1d0a3e0aaf74911a4a5d4eee39ece1
Théo Bossert <theo67.jeu@gmail.com>
2023-02-25 17:37:39 +0100

    jeu fini

--- _build/default/game.ml
+++ _build/default/game.ml
@@ -5,7 +5,7 @@ let () = print_endline "Rentrer la taille de la grille:"
 let l = read_int ()
 let () = print_endline "Rentrer le nombre de couleurs (max 8):"
 let c = read_int ()
-let a_c = [|Color.blue; Color.yellow; Color.orange; Color.green; Color.red; Color.green; Color.brown; Color.pink|]
+let a_c = [|Color.blue; Color.yellow; Color.orange; Color.magenta; Color.red; Color.green; Color.lime; Color.pink|]
 
 let init_array l =
   let g = Array.make l [|0|] in
@@ -21,19 +21,19 @@ let colors () =
   for i = 0 to l - 1 do
     for j = 0 to l - 1 do
       self_init ();
-      g.(i).(j) <- int (c - 1)
+      g.(i).(j) <- int c
     done;
   done
 
 let grid () =
   for i = 0 to l - 1 do
     for j = 0 to l - 1 do
-        draw_rectangle (i*16) (j*16) 16 16 a_c.(g.(i).(j))
+        draw_rectangle (i*32) (j*32) 32 32 a_c.(g.(i).(j))
     done;
   done
 
 let color_click () =
-  if is_mouse_button_pressed Left then g.(get_mouse_x () / 16).(get_mouse_y () / 16)
+  if is_mouse_button_pressed Left then g.(get_mouse_x () / 32).(get_mouse_y () / 32)
   else -1
 
 let grid_coloring couleur =
@@ -44,9 +44,9 @@ let grid_coloring couleur =
       g.(x).(y) <- couleur;
       if x = 0 && y = 0 then(aux 1 0; aux 0 1;)
       else if x = (l - 1) && y = (l - 1) then(aux (l - 2) (l - 1); aux (l - 1) (l - 2);)
-      else if x = (l - 1) && y = 0 then(aux (l - 2) 0; aux (l - 1) l;)
+      else if x = (l - 1) && y = 0 then(aux (l - 2) 0; aux (l - 1) 1;)
       else if x = 0 && y = (l - 1) then(aux 1 (l - 1); aux 0 (l - 2);)
-      else if y = (l - 1) then(aux (x - 1) (l - 1); aux (x + 1) (l - 1); aux x (l - 2);)
+      else if y = (l - 1) then(aux (x - 1) y; aux (x + 1) y; aux x (y - 1);)
       else if x = (l - 1) then(aux x (y - 1); aux x (y + 1); aux (x - 1) y;)
       else if x = 0 then(aux 1 y; aux 0 (y - 1); aux 0 (y + 1);)
       else if y = 0 then(aux x 1; aux (x - 1) 0; aux (x + 1) 0;)
@@ -56,7 +56,7 @@ let grid_coloring couleur =
 
 
 let setup () =
-  init_window (l*16) (l*16) "Flood it";
+  init_window (l*32) (l*32) "Flood it";
   set_target_fps 60;
   colors ();
   begin_drawing ();
